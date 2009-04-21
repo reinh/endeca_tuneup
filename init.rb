9b3ef0e4d89164ba1648465cdf1a09bd02fb1546
ReinH <reinh@yoda.corp.hpcinteractive.com>
2009-04-21 11:05:43 -0400

    Changing label

--- init.rb
+++ init.rb
@@ -2,21 +2,21 @@ if defined? Endeca && defined? Endeca::Document
   # Tuneup instrumentation
   class << Endeca::Document
     def all_with_tuneup(*args)
-      Fiveruns::Tuneup.step "#{name}.all(#{args.inspect})", :model do
+      Fiveruns::Tuneup.step "#{name}.all", :model do
         all_without_tuneup(*args)
       end
     end
     alias_method_chain :all, :tuneup
 
     def first_with_tuneup(*args)
-      Fiveruns::Tuneup.step "#{name}.first(#{args.inspect})", :model do
+      Fiveruns::Tuneup.step "#{name}.first", :model do
         first_without_tuneup(*args)
       end
     end
     alias_method_chain :first, :tuneup
 
     def by_id_with_tuneup(*args)
-      Fiveruns::Tuneup.step "#{name}.by_id(#{args.inspect})", :model do
+      Fiveruns::Tuneup.step "#{name}.by_id", :model do
         by_id_without_tuneup(*args)
       end
     end
